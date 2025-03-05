26a0803363488ecc608c3fad2574c744e3bcffe7
MrUmbra <cloudhoptech@gmail.com>
2025-03-05 11:32:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
