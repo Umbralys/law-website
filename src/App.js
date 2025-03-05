0d874804a49c205a23592a7eff0add78050cad1d
MrUmbra <cloudhoptech@gmail.com>
2025-03-05 12:12:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import LawTypes from './pages/LawTypes';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-
 function App() {
   return (
-    <Router basename="/law-website">
+    <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
